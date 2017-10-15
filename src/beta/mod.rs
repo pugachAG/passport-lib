6ec3cc9c2fac5d2e2d964959b6cddc0b76f95d4b
Anton Puhach <pugachag@gmail.com>
2017-10-15 17:55:35 +0200

    Moar code

--- src/beta/mod.rs
+++ src/beta/mod.rs
@@ -1,8 +1,23 @@
 mod beta {
-    enum Literal {
-    }
-
     enum RuleExpression {
+        Literal(Box<LiteralExpression>),
+        Operator(Box<OperatorExpression>),
     }
 
+    enum LiteralExpression {
+        Numeric(i32),
+        String(String),
+        Boolean(bool),
+    }
+
+    struct OperatorExpression {
+        operator_type: OperatorType,
+        args: Vec<RuleExpression>,
+    }
+
+    enum OperatorType {
+        And,
+        Or,
+        List,
+    }
 }
